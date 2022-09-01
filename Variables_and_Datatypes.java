6f316b7e9d6fe960ad08f127271159808e68e160
raz <48359006+rajroshansharma@users.noreply.github.com>
2022-09-01 12:42:51 +0530

    Update Variables_and_Datatypes.java

--- Variables_and_Datatypes.java
+++ Variables_and_Datatypes.java
@@ -1,5 +1,34 @@
 public class Main {
 	public static void main(String[] args) {
+		/*There are two types of variables types primitive and object type.
+		like one is normal and another is of class type example
+		there is a integer class and all class datatype starts with a 
+		capital letter.*/
+		
+		// primitive type
+		String hello = "hello";
+		char a = 'a';
+		byte b = 10;
+		short s = 20;
+		int i = 5000;
+		long l = 65890890;
+		float f = 50.0F;
+		double d = 60.02D;
+		boolean bo = true;
+		
+		// object type
+		Integer io = new Integer(67);
+		Integer ioo = i;
+		
+		Byte bo = b;
+		Short so = s;
+		Long lo = l;
+		Float fo = f;
+		Double doo = d;
+		Boolean boo = bo;
+		
+		// converting primitive to objects called Autoboxing and vice versa Unboxing
+		System.out.println(ioo);
 		// primitives : byte = 1 bytes, short = 2 bytes, int = 4 bytes, long,flaot, double,boolean,char = 2 bytes
 		// wrapper : Byte, Short, Integer, Long, double (Classe)
 		int num;
@@ -23,5 +52,6 @@ public class Main {
 		
 		char ch;
 		ch = 
+			
 	}
 }
